217fd14c1d00767b2e60d95a15e1721408acc9b2
Muhammet <muhammetuzun5@gmail.com>
2020-05-17 14:49:01 +0300

    Update

--- 1.Svn/Client/UserInterface/PythonNetworkStreamModule.cpp
+++ 1.Svn/Client/UserInterface/PythonNetworkStreamModule.cpp
@@ -4,24 +4,31 @@
 #include "PythonBackground.h"
 PyObject* netGetTipInfo(PyObject* poSelf, PyObject* poArgs)
 {
-	std::string Find("Normal");
-	if (!NS_TipList::Tip_List.empty()) {
-		const auto it = std::find_if(NS_TipList::Tip_List.begin(), NS_TipList::Tip_List.end(), [](const std::pair<std::string, std::pair<std::vector<int>, std::vector<long>>>& t) {return (std::find(std::get<NS_TipList::MAP_VEC>(t.second).begin(), std::get<NS_TipList::MAP_VEC>(t.second).end(), CPythonBackground::Instance().GetWarpMapIndex()) != std::get<NS_TipList::MAP_VEC>(t.second).end()); });
-		if (it != NS_TipList::Tip_List.end())
-			Find = it->first;
+	auto Find("");
+	const auto idx = CPythonBackground::Instance().GetWarpMapIndex();
+	using namespace NS_TipList;
+
+	if (!Tip_List.empty()) {
+		const auto it = std::find_if(Tip_List.begin(), Tip_List.end(), [&idx](const decltype(Tip_List)::value_type& Tlist) {
+			const auto& MapVec = std::get<MAP_VEC>(Tlist);
+			return (std::find(MapVec.begin(), MapVec.end(), idx) != MapVec.end());
+		});
+
+		const auto& Tip_Tuple = (it != Tip_List.end()) ? *it : *Tip_List.begin();
 
 		try {
-			const auto& VnumVec = std::get<NS_TipList::VNUM_VEC>(NS_TipList::Tip_List.at(Find));
+			const auto& VnumVec = std::get<VNUM_VEC>(Tip_Tuple);
 			const auto& idx = VnumVec.at(random_range(0, VnumVec.size() - 1));
-			Find = CPythonNetworkStream::Instance().GetTipMap().at(idx);
-		}		
-		catch (const std::out_of_range& ex) {
+			Find = CPythonNetworkStream::Instance().GetTipMap().at(idx).c_str();
+		}
+		catch (const std::out_of_range & ex) {
 			TraceError("netGetTipInfo error:: %s", ex.what());
-			TraceError("netGetTipInfo---> Find:%s ", Find.c_str());
+			TraceError("netGetTipInfo WarpMapIndex:: %ld", idx);
+			TraceError("netGetTipInfo---> Find:%s ", std::get<NAMESTR>(Tip_Tuple));
 		}
 	}
 
-	return Py_BuildValue("s", Find.c_str());
+	return Py_BuildValue("s", Find);
 }
 #endif
 
